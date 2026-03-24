b9d4df0a080241cb886ea9d4a02fa6c1f896028b
EdwinLrw <edwinlrwhs@gmail.com>
2026-03-24 01:47:31 -0700

    remove duplicate baybuild folder

--- baybuild/app/lib/supabase.ts
+++
@@ -1,6 +0,0 @@
-import { createClient } from "@supabase/supabase-js"
-
-const supabaseUrl = "https://jkexmeoduleieibebtnf.supabase.co"
-const supabaseAnonKey = "sb_publishable_II8BtIkvHUDRQu0ST0HeeA_P3cqy6U4"
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
